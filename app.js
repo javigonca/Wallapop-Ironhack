65299d97f71cf72366f94e8a626a128370d5f0cf
Ander Zulaika <anderzu85@gmail.com>
2023-02-12 19:06:17 +0100

    first structure

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+
+const app = express ();
+
+
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`);
+
+const routes = require('./config/routes.config');
+app.use('/', routes)
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.info(`App listening at port ${port}`))
---
+++ config/routes.config.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const common = require("../controllers/common.controller");
+
+const router = express.Router();
+
+router.get("/", common.home),
+
+module.exports = router;
